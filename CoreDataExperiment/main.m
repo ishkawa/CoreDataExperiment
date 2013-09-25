7c4f1b1924831fc8a5552142d7e7111f4bb75f67
Yosuke Ishikawa <y@ishkawa.org>
2013-09-26 01:21:45 +0900

    Initial Commit

---
+++ CoreDataExperiment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataExperiment
+//
+//  Created by Yosuke Ishikawa on 9/26/13.
+//  Copyright (c) 2013 Yosuke Ishikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDEAppDelegate class]));
+    }
+}
